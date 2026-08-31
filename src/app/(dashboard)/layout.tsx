49917fb1de4ad381c6ccd64bd4fb8fb63352c5fa
Taqiyyah Adha <taqiyyhdh@gmail.com>
2026-08-31 14:04:45 +0700

    feat: add sidebar

--- src/app/(dashboard)/layout.tsx
+++ src/app/(dashboard)/layout.tsx
@@ -7,6 +7,7 @@ export default function DashboardLayout({
 }) {
   return (
     <div className="min-h-screen lg:flex">
+      <Sidebar />
       <main className="min-w-0 flex-1 p-4 sm:p-6 lg:p-8">
         {children}
       </main>
