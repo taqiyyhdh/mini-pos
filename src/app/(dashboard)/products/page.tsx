dda61e5f004c00f0043885bf9b485078fc61ac6a
Taqiyyah Adha <taqiyyhdh@gmail.com>
2026-09-01 11:37:28 +0700

    feat: add product page

--- src/app/(dashboard)/products/page.tsx
+++ src/app/(dashboard)/products/page.tsx
@@ -1,8 +1,48 @@
+"use client";
+import Link from "next/link";
+import { useState } from "react";
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { EmptyState } from "@/components/ui/empty-state";
+import {Plus} from "lucide-react";
+
 export default function ProductsPage() {
+  const [search, setSearch] = useState("");
   return (
     <div>
-      <h1>Produk</h1>
-      <p>Halaman produk MiniPOS</p>
+      <div className="mb-6 flex flex-col gap-4 sm:flex-row sm:items-end sm:justify-between">
+        <div>
+          <p className="text-sm font-bold text-indigo-600">
+            MASTER DATA
+          </p>
+
+          <h1 className="mt-1 text-3xl font-black tracking-tight">
+            Produk
+          </h1>
+
+          <p className="mt-2 text-sm text-slate-500">
+            Kelola produk, harga, dan stok.
+          </p>
+        </div>
+
+        <Link href="/products/create">
+          <Button className="w-full sm:w-auto">
+            <Plus size={18} />
+            Tambah Produk
+          </Button>
+        </Link>
+      </div>
+      <div className="mb-5 max-w-md">
+        <Input
+          placeholder="Cari nama atau SKU..."
+          value={search}
+          onChange={(e) => setSearch(e.target.value)}
+          />
+      </div>
+      <EmptyState 
+        title="Belum ada produk"
+        description="Tambahkan produk pertama untuk memulai transaksi POS."
+      />
     </div>
   );
 }
